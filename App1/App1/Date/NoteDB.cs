fefda568d6de44e1239c387214dcb65d9da22eed
home <home@GARKAVAPC>
2023-10-31 20:49:13 +0200

    Добавьте файлы проекта.

---
+++ App1/App1/Date/NoteDB.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+using SQLite;
+using Notes.Models;
+using System.Threading.Tasks;
+
+namespace Notes.Date
+{
+    public class NotesDB
+    {
+        readonly SQLiteAsyncConnection db;
+
+        public NotesDB(string ConnectionString)
+        {
+            db = new SQLiteAsyncConnection(ConnectionString);
+
+            db.CreateTableAsync<Note>().Wait();
+        }
+
+
+        // возвращает все списки
+        public Task<List<Note>> GetNotesAsync()
+        {
+            return db.Table<Note>().ToListAsync();
+        }
+
+        // возвращает 1 список
+        public Task<Note> GetNoteAsync(int id)
+        {
+            return db.Table<Note>()
+                            .Where(i => i.ID == id)
+                            .FirstOrDefaultAsync();
+        }
+
+
+        // добавление/изменение уже сухествующего списка
+        public Task<int> SaveNoteAsync(Note note)
+        {
+            if (note.ID != 0) // есть ли список
+            { // есть
+                return db.UpdateAsync(note); // обновление списка
+            }
+            else
+            { // нет
+                return db.InsertAsync(note); // добавление списка
+            }
+        }
+
+        // удаление списка
+        public Task<int> DeleteNoteAsync(Note note)
+        {
+            return db.DeleteAsync(note);
+        }
+    }
+}
---
+++ App1/App1/Views/NoteAddingPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Notes.Models;
+
+namespace App1.Views
+{
+    [QueryProperty(nameof(ItemId), nameof(ItemId))]
+    public partial class NoteAddingPage : ContentPage
+    {
+        public string ItemId
+        {
+            set
+            {
+                LoadNote(value);
+            }
+        }
+
+
+        public NoteAddingPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new Note();
+        }
+
+        private async void LoadNote(string value)
+        {
+            try
+            {
+                int id = Convert.ToInt32(value);
+
+                Note note = await App.NotesDB.GetNoteAsync(id);
+
+                BindingContext = note;
+            }
+            catch { }
+        }
+
+        private static string[] pas = { 
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+                "m", "n", "o", "p", "q", "r", "s", "t", "v", "u", "w", "x", "y", "z",
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+                "O", "P", "Q", "R", "S", "T", "V", "U", "W", "X", "V", "Z",
+                "!", "@", "#", "$", "%", "+", "&", "*", "-", "_", "/",
+                "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+        };
+        private async void OnSaveButton_Clicked(object sender, EventArgs e)
+        {
+            Note note = (Note)BindingContext;
+
+            note.Date = DateTime.Now;
+
+            var rand = new Random();
+            string newPas = "";
+            for (int i = 0; i < 10; i++)
+                newPas += pas[rand.Next(0,pas.Length)];
+            note.Login = newPas;
+
+            if (!string.IsNullOrWhiteSpace(note.Text))
+            {
+                await App.NotesDB.SaveNoteAsync(note);
+            }
+
+            await Shell.Current.GoToAsync("..");
+        }
+
+        private async void OnDeleteButton_Clicked_1(object sender, EventArgs e)
+        {
+            Note note = (Note)BindingContext;
+
+            await App.NotesDB.DeleteNoteAsync(note);
+
+            await Shell.Current.GoToAsync("..");
+        }
+    }
+}
